8e76ac09b3646a5e48beb76d2076c911aa83e9f7
Telmo Barros <telmo.20@hotmail.com>
2017-03-02 14:38:11 +0000

    first commit by Telmo

---
+++ src/sdis1/Main.java
@@ -0,0 +1,16 @@
+package sdis1;
+
+public class Main {
+
+	/**
+	 * Main service starter
+	 * @param args Server arguments by the following order:
+	 * 1 - Server ID
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
