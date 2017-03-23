49a5a5ef1446c13af986c90f8f733eacb76b16dd
Antonio-Melo <happyd3nt@gmail.com>
2017-03-23 18:35:41 +0000

    Starting Backup

--- src/server/protocol/Backup.java
+++ src/server/protocol/Backup.java
@@ -1,14 +1,41 @@
 package server.protocol;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Backup {
-	
-	 public Backup(String filePath, int replicationDeg){
-		 //TODO 
-		 //Get file size from filePath
-		 //Divide file in chunks
-		 //Call PutChunk for each chunk
+
+	public Backup(String filePath, int replicationDeg) {
+		//TODO
+
+		File f = new File(filePath);
+		long fileLength = f.length();
+		byte[] chunk = new byte[64000];
 		
-		 
-	 }
+		try {
+			InputStream in = new FileInputStream(f);
+			
+			int numChunks = (int)(fileLength/64000)+1;
+			int lastChunkSize = (int)(fileLength%64000);
+			for(int i = 0;i < numChunks;i++){
+				if(i == numChunks-1){
+					chunk = new byte[lastChunkSize];
+				}
+				int numbytesRead = in.read(chunk, i*64000, 64000);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
 
 }
--- src/server/task/initiatorPeer/PutChunk.java
+++ src/server/task/initiatorPeer/PutChunk.java
@@ -1,5 +1,12 @@
 package server.task.initiatorPeer;
 
-public class PutChunk {
+public class PutChunk implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
