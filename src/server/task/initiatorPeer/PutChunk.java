be60d83f2ef97186c82103c1d487870edae5b6e9
Antonio-Melo <happyd3nt@gmail.com>
2017-03-24 20:39:53 +0000

    Doing putchunk init-peer

--- src/server/task/initiatorPeer/PutChunk.java
+++ src/server/task/initiatorPeer/PutChunk.java
@@ -1,6 +1,27 @@
 package server.task.initiatorPeer;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
+
+import server.main.Peer;
+import utils.Utils;
+
 public class PutChunk implements Runnable{
+	
+	public static void main(String[] args){
+		byte[] carlos = {(byte) 0xff,(byte)0xff, (byte)0xff};
+		new Thread(new PutChunk(
+				"1.0",
+				3,
+				"PutChunk_teste_initPeer",
+				10,
+				1,
+				carlos
+				)).start();
+	}
 	
 	private String version;
 	private int senderID;
@@ -20,7 +41,31 @@ public class PutChunk implements Runnable{
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		// TODO 
+		// Generate String with chunks and send it to the multicast channel
+		InetAddress mdbGroup;
+		try {
+			mdbGroup = InetAddress.getByName(Peer.mdbAddress);
+			MulticastSocket socket = new MulticastSocket(Peer.mdbPort);
+			socket.joinGroup(mdbGroup);
+			//SEND PUTCHUNK msg
+			byte[] header = new String("PUTCHUNK "+ this.version + Utils.Space+ this.senderID + Utils.Space + this.fileID+ Utils.Space+ this.chunkNo+ Utils.Space + this.replicationDegree+Utils.Space+Utils.CRLF+Utils.CRLF).getBytes();
+			byte[] chunk = new byte[header.length + this.body.length];
+			System.arraycopy(header, 0, chunk, 0, header.length);
+			System.arraycopy(this.body, 0, chunk, header.length, this.body.length);
+			
+
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 	}
 
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -4,6 +4,7 @@ public final class Utils {
 
 	public static final String FS = System.getProperty("file.separator");
 	public static final String CRLF = "\r\n";
+	public static final String Space = " ";
 	
 	public static final String getFileID(String FilePath){
 		return "1";
