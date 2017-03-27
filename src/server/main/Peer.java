9a74d33d6f91fb897e20f99c6246062d185b4e6b
Antonio-Melo <happyd3nt@gmail.com>
2017-03-27 17:01:34 +0100

    Change peer args order

--- src/server/main/Peer.java
+++ src/server/main/Peer.java
@@ -12,7 +12,8 @@ import server.protocol.ClientInterface;
 import utils.Utils;
 
 public class Peer {
-
+	
+	public static String protocolVersion = "1.0";
 	public static int serverID = 1;
 	public static String mcAddress = "224.0.0.1";
 	public static int mcPort = 9001;
@@ -28,75 +29,73 @@ public class Peer {
 	/**
 	 * Main service starter
 	 * @param args Server arguments by the following order:
-	 * 1 - Server ID
-	 * 2 - MC Address
-	 * 3 - MC Port
-	 * 4 - MDB Address
-	 * 5 - MDB Port
-	 * 6 - MDR Address
-	 * 7 - MDR Port
-	 * 8 - RMI Remote Object name
+	 * 1 - Protocol Version
+	 * 2 - Server ID
+	 * 3 - RMI Remote Object name
+	 * 4 - MC Address
+	 * 5 - MC Port
+	 * 6 - MDB Address
+	 * 7 - MDB Port
+	 * 8 - MDR Address
+	 * 9 - MDR Port
 	 */
 	public static void main(String[] args) {
 		if(args.length != 7 && args.length != 9){
-			System.out.println("Usage: Peer <Server ID> <MC Address> <MC Port> <MDB Address> <MDB Port> <MDR Address> <MDR Port> [<Data Path> <RMI Remote Object Name>]");
+			System.out.println("Usage: Peer <Protocol Version> <Server ID> <RMI Remote Object Name> <MC Address> <MC Port> <MDB Address> <MDB Port> <MDR Address> <MDR Port>");
 			return;
 		}
-
-		serverID = Integer.parseInt(args[0]);
+		
+		protocolVersion = args[0];
+		if(protocolVersion == null){
+			System.out.println("Null protocol version");
+		}
+		serverID = Integer.parseInt(args[1]);
 		if (serverID < 0){
 			System.out.println("Server ID wrong!");
 			return;
 		}
+		remoteObject = args[2];
+		if (remoteObject == null){
+			System.out.println("Remote Object Name wrong!");
+			return;
+		}
 
-		mcAddress = args[1];
+		mcAddress = args[3];
 		if (mcAddress == null){
 			System.out.println("MC Address wrong!");
 			return;
 		}
 
-		mcPort = Integer.parseInt(args[2]);
+		mcPort = Integer.parseInt(args[4]);
 		if (mcPort < 1024){
 			System.out.println("MC Port wrong!");
 			return;
 		}
-		mdbAddress = args[3];
+		mdbAddress = args[5];
 		if (mdbAddress == null){
 			System.out.println("MDB Address wrong!");
 			return;
 		}
 
-		mdbPort = Integer.parseInt(args[4]);
+		mdbPort = Integer.parseInt(args[6]);
 		if (mdbPort < 1024){
 			System.out.println("MDB Port wrong!");
 			return;
 		}
 
-		mdrAddress = args[5];
+		mdrAddress = args[7];
 		if (mdrAddress == null){
 			System.out.println("MDR Address wrong!");
 			return;
 		}
 
-		mdrPort = Integer.parseInt(args[6]);
+		mdrPort = Integer.parseInt(args[8]);
 		if (mdrPort < 1024){
 			System.out.println("MDR Port wrong!");
 			return;
 		}
-
-		if(args.length == 9){
-			dataPath = args[7];
-			if (dataPath == null){
-				System.out.println("Data Path wrong!");
-				return;
-			}
-
-			remoteObject = args[8];
-			if (remoteObject == null){
-				System.out.println("Remote Object Name wrong!");
-				return;
-			}
-		}
+		//Coisas parvas do telmo
+		dataPath = args[9];
 
 		System.out.println("Starting services...");
 
