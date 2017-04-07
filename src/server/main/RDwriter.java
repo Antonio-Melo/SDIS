4ac628c161b18321f77b383d8d981864ae2d647a
Telmo Barros <telmo.20@hotmail.com>
2017-04-07 17:18:16 +0100

    finished

---
+++ src/server/main/RDwriter.java
@@ -0,0 +1,24 @@
+package server.main;
+
+import utils.Utils;
+
+public class RDwriter implements Runnable {
+    @Override
+    public void run()
+    {
+        while(!Thread.currentThread().isInterrupted()) {
+           System.out.println("Thread is doing something");
+           if(Peer.saveRD == true){
+             Utils.saveRD();
+             Peer.saveRD = false;
+           }
+           try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        }
+    }
+
+}
